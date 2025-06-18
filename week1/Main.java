53687c58b6a3bf2a678188ef0faf6e8484d40b45
Samantha <27nguyens28@stu.smuhsd.org>
2025-06-18 16:07:07 -0700

    added comment

---
+++ week1/Main.java
@@ -0,0 +1,13 @@
+package progtraining2026.week1;
+
+// hi freshies your so sigma
+public class Main{
+
+// make a new comment that says something
+// hi my name is samantha
+
+public static void main (String [] args){
+    System.out.print("Hi World.");
+}
+
+}
