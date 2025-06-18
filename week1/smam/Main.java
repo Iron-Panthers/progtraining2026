40c2947c81ac409a536c8050cf7ba6f7d3226aa3
Samantha <27nguyens28@stu.smuhsd.org>
2025-06-18 15:31:21 -0700

    solution!!!
    myt first gitty pushhh

---
+++ week1/smam/Main.java
@@ -0,0 +1,12 @@
+package progtraining2026.week1.smam;
+
+// hi freshies your so sigma
+public class Main{
+
+    public static void main (String [] args){
+        // print "I eat cereal"
+        System.out.print("I want to eat cereat.");
+
+    }
+
+}
