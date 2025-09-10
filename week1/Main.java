8c2b20479e8e6675de74280596010e45c9fc7c1b
Christopher Chang <christopherchang234@gmail.com>
2025-09-09 18:30:42 -0700

    Finished mini scanner project

--- week1/Main.java
+++ week1/Main.java
@@ -1,5 +1,7 @@
 package progtraining2026.week1;
 
+import java.util.Scanner;
+
 // hi freshies your so sigma
 public class Main{
 
@@ -7,7 +9,31 @@ public class Main{
 // hi my name is samantha
 
 public static void main (String [] args){
-    System.out.print("Hi World.");
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Pick a random number 1-100");
+    int age = input.nextInt();
+
+    System.out.println("Pick another random number 1-100");
+    int place = input.nextInt();
+    input.nextLine();
+
+    System.out.println("Name a random adjective to describe a puppy.");
+    String adj = input.nextLine();
+
+    System.out.println("Name a random city.");
+    String city = input.nextLine();
+
+    System.out.println("Name a random family member. (Ex. mom)");
+    String fam = input.nextLine();
+
+    System.out.println("Name a country.");
+    String country = input.nextLine();
+
+    System.out.println("Name an action that you do daily. (Ex. walk)");
+    String verb = input.nextLine();
+
+    System.out.println("Your " + age + "-year-old " + fam + " "+ verb + "ed for hours and hours because they were extremely " + adj + ". They " + verb + "ed so well that "+ country + " chose them to represent the entire country at the Olympics, held in " + city+". Unfortunately, they placed " + place + "th. :(");
 }
 
 }
