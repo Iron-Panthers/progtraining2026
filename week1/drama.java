885b65d5404f9957f79b8bec474d7c8c39e27ea5
Alisa Leung <alisal.singyee@gmail.com>
2025-09-11 19:08:30 -0700

    updated conditionals project :D

--- week1/drama.java
+++ week1/drama.java
@@ -5,15 +5,74 @@ public class drama {
         int children = 5;
         int student = 18;
         int adult = 60;
+        String ageGroup;
+
         System.out.println("welcome to the movie theater: how old r u");
         int age = input.nextInt();
-        boolean isChild = age <= children;
-        boolean isStudent = age > children && age <= student;
-        boolean isAdult = age > student && age <= adult;
-        boolean isSenior = age > adult;
-        System.out.println("child: " + isChild);
-        System.out.println("student: " + isStudent);
-        System.out.println("adult: " + isAdult);
-        System.out.println("senior: " + isSenior);
+        int price = 0;
+        if (age <= children) {
+            price = 5;
+            ageGroup = "child";
+            System.out.print("child: " + price);
+        } else if (age <= student) {
+            price = 10;
+            System.out.print("student: " + price);
+            ageGroup = "student";
+        } else if (age <= adult) {
+            price = 15;
+            System.out.print("adult: " + price);
+            ageGroup = "adult";
+        } else {
+            price = 10;
+            System.out.print("senior: " + price);
+            ageGroup = "senior";
         }
+
+        System.out.println("is it ur bday");
+        boolean bday = input.nextBoolean();
+        if (bday) {
+            price -= 3;
+            System.out.println("happy bday! u get a discount! of 3 dollars off");
+            System.out.println("price: " + price);
+        }
+        
+        String promo = "robotics";
+        boolean hasPromo = input.nextBoolean();
+        if (hasPromo) {
+            System.out.println("input ur promo code");
+            String userPromo = input.nextLine();
+            if (userPromo.equals(promo)){
+                price -= 2;
+                System.out.println("yay! u get a discount of 2 dollars off");
+                System.out.println("price: " + price);
+            } else {
+                System.out.println("sorry, that promo code is invalid");
+                System.out.println("price: " + price);
+            }
+        }
+        System.out.println("are u a member of cast");
+        boolean isCast = input.nextBoolean();
+        if (isCast) {
+            if (ageGroup.equals("child")){
+                System.out.print("do u want free rotten tomatoes");
+                boolean tomatoes = input.nextBoolean();
+                if (tomatoes){
+                    System.out.println("yay u get free rotten tomatoes");
+                } else{
+                    System.out.println("ok no free rotten tomatoes");
+                }
+            } else if (ageGroup.equals("adult") || ageGroup.equals("senior")){
+                System.out.print("do u want flowers for an extra $5");
+                boolean flowers = input.nextBoolean();
+                if (flowers){
+                    price += 5;
+                    System.out.println("ok u get flowers");
+                    System.out.println("price: " + price);
+                } else{
+                    System.out.println("ok no flowers");
+                }
+            }
+        }
+        input.close();
     }
+}
