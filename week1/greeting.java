16b51a925111e47c21a8069dbfa4e337a6c17dc5
Alisa Leung <alisal.singyee@gmail.com>
2025-09-16 18:05:01 -0700

    personalized greeting project

---
+++ week1/greeting.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+public class greeting {
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int num = rand.nextInt(6);
+        System.out.println("whats ur name");
+        String name = input.nextLine();
+        switch (num){
+            case 0:
+                System.out.println("hi " + name);
+            case 1:
+                System.out.println("hello " + name);
+                break;
+            case 2:
+                System.out.println("greetings " + name);
+                break;
+            case 3:
+                System.out.println("salutations " + name);
+                break;
+            case 4:
+                System.out.println("hey there " + name);
+                break;
+            default:
+                System.out.println("hi there " + name);
+                break;
+        }
+    }
+}
