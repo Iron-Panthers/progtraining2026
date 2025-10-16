81803cd0104aa1f0e5e04dc3ec771c5f95a186e2
Robbyrossross <preston.w.wei@gmail.com>
2025-10-16 16:39:34 -0700

    Yay

--- Project.java
+++ Project.java
@@ -21,32 +21,51 @@ public class Project {
     int yCoord= scan.nextInt();
     System.out.println("Type true if u won, false if u didnt");
     boolean win = scan.nextBoolean();
-       
-      while(win==false){
-    while (xCoord<=3 && yCoord<= 3){
+    Scanner scan2 = new Scanner(System.in);
+    boolean invalid = false;
+  while(win==false){ // player has not won
+
+    if (xCoord>3 || yCoord>3){
+      invalid = true;
+    }
+
+    while (invalid){
       if (xCoord>3 || yCoord>3){
         System.out.println("Bruh, the board is only 3 by 3");
         System.out.println("Type the x-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board");
          xCoord= scan.nextInt();
+         System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board(For player2)");
+         int xCoord2 = scan2.nextInt();
         System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board");
          yCoord= scan.nextInt();
+         System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board(For player2)");
+          int yCoord2 = scan2.nextInt();
          numberArray[xCoord-1][yCoord-1] = "X";
-         System.out.println("Type true if u won, false if u didnt");
+         
+          numberArray[xCoord2-1][yCoord2-1] = "O";
+          System.out.println("Type true if u won, false if u didnt");
           win = scan.nextBoolean();
          break;
 
         //  System.out.println(numberArray[xCoord-1][yCoord-1]);
-      } else {
+      } 
+      
 
         numberArray[xCoord-1][yCoord-1] = "X";
         System.out.println("Type true if u won, false if u didnt");
          win = scan.nextBoolean();
          System.out.println("Type the x-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board");
          xCoord= scan.nextInt();
+         System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board(For player2)");
+         int xCoord2 = scan2.nextInt();
         System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board");
          yCoord= scan.nextInt();
+          System.out.println("Type da y-coordinate of the place u wanna place ur mark on the 3 by 3 tic tac toe board(For player2)");
+            int yCoord2 = scan2.nextInt();
+          numberArray[xCoord2-1][yCoord2-1] = "O";
+          invalid = true;
         break;
-        }
+          
     }   
     for (int i = 0; i < numberArray.length; i++) {
       for (int j = 0; j < numberArray[i].length; j++){
@@ -57,10 +76,6 @@ public class Project {
 
     //  System.out.println(numberArray[xCoord-1][yCoord-1]);
     //  numberArray[xCoord-1 ][yCoord-1 ] = "X";
-    
-    
-      
-  
   }
   
       
