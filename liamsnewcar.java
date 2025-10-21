7ffa945e989f307789e8506d1b978d783101a1a7
fivepuppies2813 <stanleyhsu73@gmail.com>
2025-10-20 17:25:32 -0700

    half finnished liams car

---
+++ liamsnewcar.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class liamsnewcar {
+    public static void main (String [] args){
+        System.out.println("Type START CAR to start the car. ");
+        Scanner input = new Scanner(System.in);
+        boolean startcar = false;
+        String START = input.nextLine();
+        if (START .equals("STARTCAR")){
+            startcar = true;
+        }
+        while (startcar == true){
+            System.out.println("buttons");
+        }
+        String opentrunk = input.nextLing();
+        if (){
+
+        }
+        
+    }
+    public static String opentrunk(){
+        return "your trunk is open";
+    }
+
+}
